f4b6b2c22779655cce12d1ccf94ff99ba7bf4796
Jose Javier Escudero Gómez <doublejay93@gmail.com>
2017-04-28 13:31:49 +0200

    WebView
    La actividad inicial ya tiene el WebView que carga nuestra parte web y
    comienza a interactuar con la parte nativa. Por acabar.

--- app/src/main/java/studio/waterwell/villaapp/MainActivity.java
+++ app/src/main/java/studio/waterwell/villaapp/MainActivity.java
@@ -1,13 +1,59 @@
 package studio.waterwell.villaapp;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView browser;
 
+    // Inicia el WebView con los datos oportunos
+    private void iniciarParteWeb(){
+        RespuestaWeb respuestaWeb = new RespuestaWeb(getApplicationContext());
+
+        browser = (WebView) findViewById(R.id.browser);
+        browser.loadUrl("http://villaapp.esy.es/login.html");
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.addJavascriptInterface(respuestaWeb, "ob");
+
+        // Esto hace que se navegue por la web sin abrir navegador
+        browser.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverriceUrlLoading(WebView view, String url){
+                return false;
+            }
+        });
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iniciarParteWeb();
+    }
+
+    // Esta clase hay que crearla de verdad en un archivo java, así solo es de prueba
+    class RespuestaWeb{
+        private Context c;
+
+        public RespuestaWeb(Context c){
+            this.c = c;
+        }
+
+        @android.webkit.JavascriptInterface
+        public void respuestaApp(String msj){
+            Toast t = Toast.makeText(c, msj.toString(), Toast.LENGTH_SHORT);
+            t.show();
+        }
+
+        @android.webkit.JavascriptInterface
+        public void datosUsuario(String user, String pass){
+            // TODO: Generar un objeto usuario con estos datos, guardarlo en la bd interna de la app
+            // TODO: y mandarlo por bundle a la actividad principal
+        }
     }
 }
