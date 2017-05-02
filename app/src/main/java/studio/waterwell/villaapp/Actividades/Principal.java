332e22eee0e0afbf1d6a06b56b3250dbad1d60c0
Jose Javier Escudero Gómez <doublejay93@gmail.com>
2017-05-02 17:26:14 +0200

    Modificaciones actividad Principal
    La actividad Principal ahora soporta un navigationDrawer como menú
    lateral despegable y un menú superior (temporal) por defecto.

--- app/src/main/java/studio/waterwell/villaapp/Actividades/Principal.java
+++ app/src/main/java/studio/waterwell/villaapp/Actividades/Principal.java
@@ -1,28 +1,111 @@
 package studio.waterwell.villaapp.Actividades;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
-
 import studio.waterwell.villaapp.Modelo.Usuario;
 import studio.waterwell.villaapp.R;
 
-public class Principal extends AppCompatActivity {
+public class Principal extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    // Componentes de la vista
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private TextView prueba;
+
+    // Atributos necesarios
     private Usuario usuario;
-    private TextView aux;
 
     // TODO: Coger del bundle mandado por CargaDatosLogin el ArrayList de ubicaciones de toda la app
     private void cargarDatos(){
         Bundle bundle = getIntent().getBundleExtra("Bundle");
         usuario = bundle.getParcelable("Usuario");
-        aux = (TextView) findViewById(R.id.textoPrueba);
-        aux.setText(usuario.getUserName());
+        prueba = (TextView) findViewById(R.id.texto_prueba);
+        prueba.setText(usuario.getUserName());
+    }
+
+
+    // Crea la barra lateral que se desplaza con el dedo para moverse en opciones
+    private void cargarNavigationDrawer(){
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
         cargarDatos();
+        cargarNavigationDrawer();
+    }
+
+    // Crea el menú de arriba (por defecto, se puede quitar)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.principal, menu);
+        return true;
+    }
+
+    // Dice que pasa al clicarse una opcion del menú de arriba (por defecto, se puede quitar)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Dice que pasa al clicarse una opcion del menú de lateral (por defecto, se puede quitar)
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
